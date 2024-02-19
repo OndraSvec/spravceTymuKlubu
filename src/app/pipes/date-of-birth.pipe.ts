d4f907d61f7294c83fc2f0e54075446ef567a439
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-19 18:34:29 +0100

    Add DateOfBirth pipe

---
+++ src/app/pipes/date-of-birth.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateOfBirth',
+  standalone: true,
+})
+export class DateOfBirthPipe implements PipeTransform {
+  transform(dob: string): string {
+    return dob.split('/').reverse().join('.');
+  }
+}

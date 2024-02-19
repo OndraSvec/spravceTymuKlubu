1d18d6df0756fde6e9804bda9e7e3100aea18aac
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-19 15:42:52 +0100

    Add LineUps component

---
+++ src/app/line-ups/line-ups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line-ups',
+  standalone: true,
+  imports: [],
+  templateUrl: './line-ups.component.html',
+  styleUrl: './line-ups.component.css'
+})
+export class LineUpsComponent {
+
+}

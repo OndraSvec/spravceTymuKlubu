c64142f022fe488c0d4af48ac51d9b630ac238ce
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-19 15:44:12 +0100

    Add Teams and LineUps routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { MembersComponent } from './members/members.component';
+import { TeamsComponent } from './teams/teams.component';
+import { LineUpsComponent } from './line-ups/line-ups.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MembersComponent,
+  },
+  {
+    path: 'teams',
+    component: TeamsComponent,
+  },
+  {
+    path: 'lineups',
+    component: LineUpsComponent,
   },
   {
     path: '**',

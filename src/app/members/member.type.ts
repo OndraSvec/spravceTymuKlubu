b203f5c8974055e6b5e0403d3a7a6ec8efefa2fa
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-19 16:33:37 +0100

    Add Member type

---
+++ src/app/members/member.type.ts
@@ -0,0 +1,10 @@
+export type Member = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  teamId: string;
+  lineUpId: string;
+  lineUpPosition: string;
+  lineUpRole: string[];
+};

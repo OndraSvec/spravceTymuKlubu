a8b6fdf9a9441d7242b8bbdb2fb5224a2e55bdbf
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-19 15:41:51 +0100

    Add Members route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MembersComponent } from './members/members.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MembersComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];

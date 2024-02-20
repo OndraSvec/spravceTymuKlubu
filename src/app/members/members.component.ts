cb2cc1ba2214edc851121fafce45d499572e34fe
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-20 13:05:54 +0100

    Display an error message upon data load failure

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -2,12 +2,13 @@ import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { MatList, MatListItem } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { ClubService } from '../services/club.service';
-import { Subscription } from 'rxjs';
+import { Subscription, catchError } from 'rxjs';
 import { Member } from './member.type';
 import { AgePipe } from '../pipes/age.pipe';
 import { DateOfBirthPipe } from '../pipes/date-of-birth.pipe';
 import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
 import { AsyncPipe } from '@angular/common';
+import { ErrorComponent } from '../error/error.component';
 
 @Component({
   selector: 'app-members',
@@ -20,6 +21,7 @@ import { AsyncPipe } from '@angular/common';
     AsyncPipe,
     DateOfBirthPipe,
     LoadingSpinnerComponent,
+    ErrorComponent,
   ],
   templateUrl: './members.component.html',
   styleUrl: './members.component.css',
@@ -30,12 +32,16 @@ export class MembersComponent implements OnInit, OnDestroy {
   public members: Member[] = [];
   public columnsToDisplay = ['name', 'dob', 'age'];
   public loading = false;
+  public error = false;
 
   ngOnInit(): void {
     this.loading = true;
-    this.membersSub = this.clubService.getMembers().subscribe((members) => {
-      this.members = members;
-      this.loading = false;
+    this.membersSub = this.clubService.getMembers().subscribe({
+      next: (members) => {
+        this.members = members;
+        this.loading = false;
+      },
+      error: () => (this.error = true),
     });
   }
 

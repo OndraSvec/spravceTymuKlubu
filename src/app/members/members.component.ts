ce05f3a8f287dd3eba7527d3e69127fd78e1a557
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-20 20:20:06 +0100

    Disable editMember form if form invalid

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -67,16 +67,18 @@ export class MembersComponent implements OnInit, OnDestroy {
     const dialogRef = this.dialog.open(DialogComponent, { data: { member } });
 
     dialogRef.afterClosed().subscribe((res) => {
-      this.members = this.members.map((member) =>
-        member.id === res.data.id
-          ? {
-              ...member,
-              firstName: res.data.firstName,
-              lastName: res.data.lastName,
-              dob: res.data.dob,
-            }
-          : member
-      );
+      if (res) {
+        this.members = this.members.map((member) =>
+          member.id === res.data.id
+            ? {
+                ...member,
+                firstName: res.data.firstName,
+                lastName: res.data.lastName,
+                dob: res.data.dob,
+              }
+            : member
+        );
+      }
     });
   }
 }

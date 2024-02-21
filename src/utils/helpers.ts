0911752e944d574bb6c292df2c36a12667749b94
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-21 12:10:10 +0100

    Add sortMembers helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { Member } from '../app/members/member.type';
+
 export function datePickerFormatter(date: string | Date) {
   const parsedDate = typeof date === 'string' ? new Date(date) : date;
   const formattedDateString =
@@ -7,4 +9,12 @@ export function datePickerFormatter(date: string | Date) {
     '-' +
     parsedDate.getDate().toString().padStart(2, '0');
   return formattedDateString;
+}
+
+export function sortMembers(members: Member[]) {
+  return members.sort((a, b) => {
+    const result = a.lastName.localeCompare(b.lastName);
+
+    return result !== 0 ? result : a.firstName.localeCompare(b.firstName);
+  });
 }

e32307bd929a8368d4a979f4d9cfb4e8bed47a48
Ondra Svec <ondrasvecveslo@gmail.com>
2024-02-27 14:19:38 +0100

    Update filteredMembers onDelete

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -78,6 +78,7 @@ export class MembersComponent implements OnInit {
   onDelete(id: string) {
     this.clubService.deleteMember(id);
     this.members = this.members.filter((member) => member.id !== id);
+    this.filteredMembers = this.members;
   }
 
   // Use an arrow function so as not to lose 'this'
